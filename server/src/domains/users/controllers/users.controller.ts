86d2cd00c4e50c0439a9a61f986065148f5cf871
Hleb Haiduk <glebgaiduk.pl@gmail.com>
2021-11-09 21:23:11 +0100

    Server: bubble try catch blocks from service to user controller level

--- server/src/domains/users/controllers/users.controller.ts
+++ server/src/domains/users/controllers/users.controller.ts
@@ -1,14 +1,17 @@
 import config from '@srcPath/common/config';
-import { DBPropertyNotExistError } from '@srcPath/common/errors/DBValidation.error';
 import { NextFunction, Request, Response } from 'express';
 import usersService from '../services/users.service';
 class UsersController {
-  async getUsers(req: Request, res: Response) {
+  async getUsers(req: Request, res: Response, next: NextFunction) {
     const limit: number = req.query.limit ? +req.query.limit : 10;
     const page: number = req.query.page ? +req.query.page : 1;
 
-    const usersList = await usersService.getAll(limit, page);
-    res.status(200).json(usersList);
+    try {
+      const usersList = await usersService.getAll(limit, page);
+      res.status(200).json(usersList);
+    } catch (err) {
+      next(err);
+    }
   }
 
   async registerUser(req: Request, res: Response, next: NextFunction) {
@@ -34,32 +37,33 @@ class UsersController {
     }
   }
 
-  async getUserById(req: Request, res: Response) {
-    const user = await usersService.getById(req.params.userId);
-
-    if (user instanceof DBPropertyNotExistError) {
-      res.status(user.statusCode).json(user.getErrorObject());
-    } else {
+  async getUserById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = await usersService.getById(req.params.userId);
       res.status(200).json(user);
+    } catch (err) {
+      next(err);
     }
   }
-  async updateUserById(req: Request, res: Response) {
-    const updatedUser = await usersService.putById(req.params.userId, req.body);
 
-    if (updatedUser instanceof DBPropertyNotExistError) {
-      res.status(updatedUser.statusCode).json(updatedUser.getErrorObject());
-    } else {
+  async updateUserById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const updatedUser = await usersService.putById(
+        req.params.userId,
+        req.body
+      );
       res.status(200).json(updatedUser);
+    } catch (err) {
+      next(err);
     }
   }
 
-  async deleteUserById(req: Request, res: Response) {
-    const removedUser = await usersService.deleteById(req.params.userId);
-
-    if (removedUser instanceof DBPropertyNotExistError) {
-      res.status(removedUser.statusCode).json(removedUser.getErrorObject());
-    } else {
+  async deleteUserById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const removedUser = await usersService.deleteById(req.params.userId);
       res.status(204).json({});
+    } catch (err) {
+      next(err);
     }
   }
 }
--- server/src/domains/users/services/users.service.ts
+++ server/src/domains/users/services/users.service.ts
@@ -2,7 +2,6 @@ import config from '@srcPath/common/config';
 import dbConfig from '@srcPath/common/db/db.config';
 import { isExistingDbProperty } from '@srcPath/common/db/db.utils';
 import ApiError from '@srcPath/common/errors/api.error';
-import Logger from '@srcPath/common/loaders/logger.loader';
 import { createHashedPassword } from '@srcPath/common/utils/password.utils';
 import rolesService from '@srcPath/domains/roles/services/roles.service';
 import tokenService from '@srcPath/domains/token/services/token.service';
@@ -10,7 +9,6 @@ import { CRUD } from '@srcPath/domains/wine/wineTypes/interfaces/crud.interface'
 import { v4 } from 'uuid';
 import { UserFromDbDTO, UserTokenDTO } from '../DTOs/user.dto';
 import {
-  IUserByEmailWithPassword,
   TCreateUser,
   TDeleteUser,
   TGetUserById,
@@ -25,15 +23,11 @@ class UsersServices
   implements CRUD<TGetUsers, TGetUserById, TCreateUser, TDeleteUser, TPutUser>
 {
   async getAll(limit: number, page: number): Promise<TGetUsers> {
-    try {
-      const users = await dbConfig.query(
-        'SELECT * FROM "user" LIMIT $1 OFFSET $2',
-        [limit, --page]
-      );
-      return users.rows;
-    } catch (err) {
-      Logger.error(err);
-    }
+    const users = await dbConfig.query(
+      'SELECT * FROM "user" LIMIT $1 OFFSET $2',
+      [limit, --page]
+    );
+    return users.rows;
   }
 
   // User registration
@@ -99,77 +93,40 @@ class UsersServices
   }
 
   async getById(id: string): Promise<TGetUserById> {
-    try {
-      const isExistingUser = await isExistingDbProperty('user', 'user_id', id);
+    const isExistingUser = await isExistingDbProperty('user', 'user_id', id);
 
-      if (!isExistingUser) throw new DBPropertyNotExistError('user_id');
+    if (!isExistingUser) throw new DBPropertyNotExistError('user_id');
 
-      const user = await dbConfig.query(
-        'SELECT * FROM "user" WHERE user_id = $1',
-        [id]
-      );
-      return user.rows[0];
-    } catch (err) {
-      Logger.error(err.message);
-      return err;
-    }
+    const user = await dbConfig.query(
+      'SELECT * FROM "user" WHERE user_id = $1',
+      [id]
+    );
+    return user.rows[0];
   }
 
   async putById(
     id: string,
     { firstName, lastName, email, password, phone }: IUserFromClient
   ): Promise<TPutUser> {
-    try {
-      const isExistingUser = await isExistingDbProperty('user', 'user_id', id);
+    const isExistingUser = await isExistingDbProperty('user', 'user_id', id);
 
-      if (!isExistingUser) throw new DBPropertyNotExistError('user_id');
-      const updatedUser = await dbConfig.query(
-        'UPDATE "user" SET first_name = $1, last_name = $2, email = $3, password = $4, phone = $5 WHERE user_id = $6 RETURNING *',
-        [firstName, lastName, email, password, phone, id]
-      );
-      return updatedUser.rows[0];
-    } catch (err) {
-      Logger.error(err.message);
-      return err;
-    }
+    if (!isExistingUser) throw new DBPropertyNotExistError('user_id');
+    const updatedUser = await dbConfig.query(
+      'UPDATE "user" SET first_name = $1, last_name = $2, email = $3, password = $4, phone = $5 WHERE user_id = $6 RETURNING *',
+      [firstName, lastName, email, password, phone, id]
+    );
+    return updatedUser.rows[0];
   }
 
   async deleteById(id: string): Promise<TDeleteUser> {
-    try {
-      const isExistingUser = await isExistingDbProperty('user', 'user_id', id);
+    const isExistingUser = await isExistingDbProperty('user', 'user_id', id);
 
-      if (!isExistingUser) throw new DBPropertyNotExistError('user_id');
-      const removedUser = await dbConfig.query(
-        'DELETE FROM "user" WHERE user_id = $1',
-        [id]
-      );
-      return removedUser.rows[0];
-    } catch (err) {
-      Logger.error(err.message);
-      return err;
-    }
-  }
-
-  async getUserByEmailWithPassword(
-    email: string
-  ): Promise<IUserByEmailWithPassword> {
-    try {
-      const user = await dbConfig.query(
-        'SELECT user_id, email, password FROM "user" WHERE email = $1',
-        [email]
-      );
-
-      const { user_id } = user.rows[0];
-
-      const userAccessRole = await dbConfig.query(
-        'SELECT title FROM role WHERE role_id = (SELECT role_id FROM user_role WHERE user_id = $1)',
-        [user_id]
-      );
-
-      return { ...user.rows[0], accessRole: userAccessRole.rows[0].title };
-    } catch (err) {
-      Logger.error(err.message);
-    }
+    if (!isExistingUser) throw new DBPropertyNotExistError('user_id');
+    const removedUser = await dbConfig.query(
+      'DELETE FROM "user" WHERE user_id = $1',
+      [id]
+    );
+    return removedUser.rows[0];
   }
 }
 
